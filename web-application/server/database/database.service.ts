77eeb5c4171b17dc044ed196f007d070990e2253
Marcel Boelen <massie_boelen@hotmail.com>
2018-11-25 21:42:33 +0100

    Improved database connection methods by making them private static

--- web-application/server/database/database.service.ts
+++ web-application/server/database/database.service.ts
@@ -3,29 +3,10 @@ import { Connection, ConnectionConfig } from 'mysql';
 const mysql = require('mysql');
 
 export class DatabaseService {
-    public executeQuery<T>(query: string): Promise<T> {
-        return new Promise((resolve, reject) => {
-            // Create a database connection
-            const connection = this.createConnection();
-
-            // Execute the query
-            connection.query(query, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
-            });
-
-            // Terminate the database connection
-            this.endConnection(connection);
-        });
-    }
-
     /**
      * Starts a database connection
      */
-    private createConnection(): Connection {
+    private static createConnection(): Connection {
         const config: ConnectionConfig = {
             host: 'localhost',
             user: 'root',
@@ -39,11 +20,35 @@ export class DatabaseService {
     /**
      * Ends the database connection
      */
-    private endConnection(connection: Connection): void {
+    private static endConnection(connection: Connection): void {
         connection.end((error) => {
             if (error) {
                 console.log('DatabaseService connection terminated', error);
             }
+        });
+    }
+
+    /**
+     * Executes a MySQL query
+     * @param {string} query The query to execute
+     * @return {Promise<T>} A promise with the result in the resolve callback
+     */
+    public executeQuery<T>(query: string): Promise<T> {
+        return new Promise((resolve, reject) => {
+            // Create a database connection
+            const connection = DatabaseService.createConnection();
+
+            // Execute the query
+            connection.query(query, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+
+            // Terminate the database connection
+            DatabaseService.endConnection(connection);
         });
     }
 }
